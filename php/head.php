28fb386450ad38a694b838548f7ff8199126994c
Jun Nogata <nogajun@gmail.com>
2022-07-24 12:00:15 +0900

    add web fonts config

--- php/head.php
+++ php/head.php
@@ -3,13 +3,45 @@
 <meta name="generator" content="Bludit">
 <?php echo Theme::metaTags('title'); ?>
 <?php echo Theme::metaTags('description'); ?>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"><?php echo Theme::css('css/bootstrap.min.css'); ?>
+<style type="text/css">
+  @font-face {
+    font-family: 'Noto Sans JP Regular';
+    font-display: fallback;
+    font-style: normal;
+    font-weight: 400;
+    src: local("Noto Sans CJK JP"),
+    url(<?php echo HTML_PATH_THEME . 'font/NotoSansCJKjp-Regular.woff2'; ?>) format('woff2');
+  }
+  @font-face {
+    font-family: 'Noto Sans JP Bold';
+    font-display: fallback;
+    font-style: normal;
+    font-weight: 700;
+    src: local("Noto Sans CJK JP"),
+    url(<?php echo HTML_PATH_THEME . 'font/NotoSansCJKjp-Bold.woff2'; ?>) format('woff2');
+  }
+  @font-face {
+    font-family: 'Lato Regular';
+    font-display: fallback;
+    font-style: normal;
+    font-weight: 400;
+    src: local("Lato"),
+    url(<?php echo HTML_PATH_THEME . 'font/Lato-Regular.woff2'; ?>) format('woff2');
+  }
+  @font-face {
+    font-family: 'Lato Bold';
+    font-display: fallback;
+    font-style: normal;
+    font-weight: 700;
+    src: local("Lato"),
+    url(<?php echo HTML_PATH_THEME . 'font/Lato-Bold.woff2'; ?>) format('woff2');
+  }
+</style>
+<?php echo Theme::css('css/bootstrap.min.css'); ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 <?php echo Theme::css('css/style.css'); ?>
 <?php echo Theme::favicon('img/favicon.ico'); ?>
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo DOMAIN_THEME_IMG . 'apple-touch-icon.webp'; ?>">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo HTML_PATH_THEME_IMG . 'apple-touch-icon.webp'; ?>">
 <!-- Load Bludit Plugins: Site head -->
 <?php Theme::plugins('siteHead'); ?>
 
